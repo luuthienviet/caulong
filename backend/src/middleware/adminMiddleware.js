8ab603e13ae19658a609a884695a8abde13d6b1d
luuthienviet <ltviet.k23tt@kontum.udn.vn>
2026-05-17 21:43:52 +0700

    update

--- backend/src/middleware/adminMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const adminMiddleware = (req, res, next) => {
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Không có quyền truy cập" });
-  }
-  next();
-};
--- backend/src/routes/reviewRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getReviewsByCourt, createReview } from '../controllers/reviewController.js';
-import { authMiddleware } from '../middleware/authMiddleware.js';
-
-const router = express.Router();
-router.get('/:courtId', getReviewsByCourt);
-router.post('/', authMiddleware, createReview);
-export default router;
